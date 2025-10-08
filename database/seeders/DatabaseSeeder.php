5eeea32421265a7cf23a32fbed07ed84d4636319
barale2906 <alexanderbarajas@gmail.com>
2025-10-08 14:31:34 -0500

    Ajuste de migraciones Referidos y seguimientos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             CursoSeeder::class,
             ReferidoSeeder::class,
+            SeguimientoSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
 
