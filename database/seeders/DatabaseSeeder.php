0d38074c8ac92b666c632bcc077175b97722bc39
barale2906 <alexanderbarajas@gmail.com>
2026-01-30 23:26:23 -0500

    Prueba de acceso

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -43,7 +43,7 @@ class DatabaseSeeder extends Seeder
             LpPrecioProductoSeeder::class,
             ConceptoPagoSeeder::class,
             DescuentoSeeder::class,
-            //ReciboPagoSeeder::class,
+            ReciboPagoSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
 
